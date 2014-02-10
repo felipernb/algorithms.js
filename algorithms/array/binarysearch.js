f61c54ae3223a92fe0452a7b37b1aadee51a74c2
Felipe Ribeiro <felipernb@gmail.com>
2014-02-10 11:07:50 +0100

    MIT license

--- algorithms/array/binarysearch.js
+++ algorithms/array/binarysearch.js
@@ -1,29 +1,49 @@
-(function(module) {
-  "use strict";
+/**
+ * Copyright (C) 2014 Felipe Ribeiro
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
 
-  /**
-   * Binary Search finds elements in sorted arrays in logarithmic
-   * time (O(lg n))
-   *
-   * @param Array
-   * @param Number|String
-   *
-   * @return Boolean
-   */
-  var binarySearch = function(sortedArray, element) {
-    var init = 0,
-        end = sortedArray.length - 1;
+"use strict";
 
-    while (end >= init) {
-      var m = ((end - init) >> 1) + init;
-      if (sortedArray[m] === element) return true;
+/**
+  * Binary Search finds elements in sorted arrays in logarithmic
+  * time (O(lg n))
+  *
+  * @param Array
+  * @param Number|String
+  *
+  * @return Boolean
+  */
+var binarySearch = function(sortedArray, element) {
+  var init = 0,
+      end = sortedArray.length - 1;
 
-      if (sortedArray[m] < element) init = m + 1;
-      else end = m - 1;
-    }
+  while (end >= init) {
+    var m = ((end - init) >> 1) + init;
+    if (sortedArray[m] === element) return true;
 
-    return false;
-  };
+    if (sortedArray[m] < element) init = m + 1;
+    else end = m - 1;
+  }
 
-  module.exports = binarySearch;
-})(module);
+  return false;
+};
+
+module.exports = binarySearch;
--- algorithms/math/fibonacci.js
+++ algorithms/math/fibonacci.js
@@ -1,65 +1,85 @@
 /**
+ * Copyright (C) 2014 Felipe Ribeiro
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+/**
  * Different implementations of the Fibonacci sequence
  *
  * @author Felipe Ribeiro <felipernb@gmail.com>
  */
 
-(function(module) {
-  "use strict";
+"use strict";
 
-  /**
-   * Regular fibonacci implementation following the definition:
-   * Fib(0) = 0
-   * Fib(1) = 1
-   * Fib(n) = Fib(n-1) + Fib(n-2)
-   *
-   * @param Number
-   * @return Number
-   */
-  var fibExponential = function (n) {
-    return n < 2 ? n : fibExponential(n-1) + fibExponential(n-2);
-  };
+/**
+  * Regular fibonacci implementation following the definition:
+  * Fib(0) = 0
+  * Fib(1) = 1
+  * Fib(n) = Fib(n-1) + Fib(n-2)
+  *
+  * @param Number
+  * @return Number
+  */
+var fibExponential = function (n) {
+  return n < 2 ? n : fibExponential(n-1) + fibExponential(n-2);
+};
 
-  /**
-   * O(n) in time, O(1) in space and doesn't use recursion
-   *
-   * @param Number
-   * @return Number
-   */
-  var fibLinear = function(n) {
-    var fibNMinus2 = 0,
-        fibNMinus1 = 1,
-        fib = n;
-    for (var i = 1; i < n; i++) {
-      fib = fibNMinus1 + fibNMinus2;
-      fibNMinus2 = fibNMinus1;
-      fibNMinus1 = fib;
+/**
+  * O(n) in time, O(1) in space and doesn't use recursion
+  *
+  * @param Number
+  * @return Number
+  */
+var fibLinear = function(n) {
+  var fibNMinus2 = 0,
+      fibNMinus1 = 1,
+      fib = n;
+  for (var i = 1; i < n; i++) {
+    fib = fibNMinus1 + fibNMinus2;
+    fibNMinus2 = fibNMinus1;
+    fibNMinus1 = fib;
+  }
+  return fib;
+};
+
+/**
+  * Implementation with memoization, O(n) in time, O(n) in space
+  *
+  * @param Number
+  * @return Number
+  */
+var fibWithMemoization = (function() {
+  var cache = [0, 1];
+
+  var fib = function(n) {
+    if (cache[n] === undefined) {
+      cache[n] = fib(n-1) + fib(n-2);
     }
-    return fib;
+    return cache[n];
   };
 
-  /**
-   * Implementation with memoization, O(n) in time, O(n) in space
-   *
-   * @param Number
-   * @return Number
-   */
-  var fibWithMemoization = (function() {
-    var cache = [0, 1];
+  return fib;
+})();
 
-    var fib = function(n) {
-      if (cache[n] === undefined) {
-        cache[n] = fib(n-1) + fib(n-2);
-      }
-      return cache[n];
-    };
-
-    return fib;
-  })();
-
-  module.exports = {
-    exponential: fibExponential,
-    linear: fibLinear,
-    withMemoization: fibWithMemoization
-  };
-})(module);
+module.exports = {
+  exponential: fibExponential,
+  linear: fibLinear,
+  withMemoization: fibWithMemoization
+};
