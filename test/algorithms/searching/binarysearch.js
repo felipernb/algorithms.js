4f048f0fab31390c044c0f360db92be204d03c1d
Felipe Ribeiro <felipernb@gmail.com>
2014-02-11 23:28:40 +0100

    Move Binary Search to the 'searching' category

--- test/algorithms/searching/binarysearch.js
+++ test/algorithms/searching/binarysearch.js
@@ -19,7 +19,7 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
  */
-var binarySearch = require('../../../algorithms/array/binarysearch'),
+var binarySearch = require('../../../algorithms/searching/binarysearch'),
     assert = require('assert');
 
 describe('Binary Search', function() {
