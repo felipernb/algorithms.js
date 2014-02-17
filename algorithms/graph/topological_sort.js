86da5ed7e11870a84fba086c1eab8138e90b1856
Felipe Ribeiro <felipernb@gmail.com>
2014-02-17 22:32:26 +0100

    #23 Topological sorting for a DAG represented by an adjacency list

---
+++ algorithms/graph/topological_sort.js
@@ -0,0 +1,77 @@
+/**
+ * Copyright (C) 2014 Felipe Ribeiro
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+'use strict';
+
+var Stack = require('../../data_structures/stack');
+
+/**
+ * Sorts the edges of the DAG topologically
+ *
+ * The graph should be represented with an adjacency list, that
+ * can be expressed as:
+ *  (node1) -> (node2) -> (node4)
+ *     \-> (node3)^
+ *
+ * {
+ *  "node1": ["node2", "node3"],
+ *  "node2": ["node4"],
+ *  "node3": ["node2"]
+ *  ...
+ * }
+ *
+ * Meaning that:
+ * - "node2" and "node3" depend on "node1"
+ * - "node4" depend on node2
+ * - "node2" depend on "node3"
+ *
+ * @return Stack
+ */
+var topologicalSort = function (graph) {
+  var stack = new Stack();
+  var firstHit = {};
+  var secondHit = {};
+  var time = 0;
+
+  /**
+  * Depth first search for a directed acyclic graph (DAG)
+  */
+  var dagDFS = function (node) {
+    if (firstHit[node]) return;
+    var neighbours = graph[node];
+    firstHit[node] = ++time;
+    for (var i = 0; i < neighbours.length; i++) {
+      dagDFS(neighbours[i]);
+    }
+    secondHit[node] = ++time;
+    stack.push(node);
+  };
+
+  for (var node in graph) {
+    if (graph.hasOwnProperty(node) && !secondHit[node]) {
+      dagDFS(node);
+    }
+  }
+
+  return stack;
+};
+
+module.exports = topologicalSort;
