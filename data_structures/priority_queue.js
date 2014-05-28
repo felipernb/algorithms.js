f7bcd54f590ef8c1ae73142452b9e95024711b03
Felipe Ribeiro <felipernb@gmail.com>
2014-05-28 23:44:54 +0200

    Priority Queue

---
+++ data_structures/priority_queue.js
@@ -0,0 +1,52 @@
+/**
+ * Copyright (C) 2014 Felipe Ribeiro
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+'use strict';
+
+var MinHeap = require('./heap').MinHeap;
+
+/**
+ * Extends the MinHeap with the only difference that
+ * the heap operations are performed based on the priority of the element
+ * and not on the element itself
+ */
+function PriorityQueue() {
+  MinHeap.call(this, function (a, b) {
+    if (a.priority === b.priority) return 0;
+    return a.priority < b.priority ? -1 : 1;
+  });
+}
+
+PriorityQueue.prototype = new MinHeap();
+
+PriorityQueue.prototype.insert = function (item, priority) {
+  MinHeap.prototype.insert.call(this, {
+    item: item,
+    priority: priority
+  });
+};
+
+PriorityQueue.prototype.extract = function () {
+  var min = MinHeap.prototype.extract.call(this);
+  return min && min.item;
+};
+
+module.exports = PriorityQueue;
---
+++ test/data_structures/priority_queue.js
@@ -0,0 +1,60 @@
+/**
+ * Copyright (C) 2014 Felipe Ribeiro
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+'use strict';
+
+var PriorityQueue = require('../../data_structures/priority_queue'),
+    assert = require('assert');
+
+describe('Min Priority Queue', function () {
+  it('should always return the element with the lowest priority', function () {
+    var q = new PriorityQueue();
+    assert(q.isEmpty());
+    q.insert('a', 10);
+    q.insert('b', 2091);
+    q.insert('c', 4);
+    q.insert('d', 1);
+    q.insert('e', 5);
+    q.insert('f', 500);
+    q.insert('g', 0);
+    q.insert('h', 18);
+    q.insert('i', 3);
+    q.insert('j', 22);
+    q.insert('k', 20);
+    assert(!q.isEmpty());
+
+    assert.equal(q.extract(), 'g');
+    assert.equal(q.extract(), 'd');
+    assert.equal(q.extract(), 'i');
+    assert.equal(q.extract(), 'c');
+    assert.equal(q.extract(), 'e');
+    assert.equal(q.extract(), 'a');
+    assert.equal(q.extract(), 'h');
+    assert.equal(q.extract(), 'k');
+    assert.equal(q.extract(), 'j');
+    assert.equal(q.extract(), 'f');
+    assert.equal(q.extract(), 'b');
+
+    assert(q.isEmpty());
+  });
+});
+
+
