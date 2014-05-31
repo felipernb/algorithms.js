dadab1b2fa4f75ebbc52b33258f1e379e24b1744
geakstr <geakstr@me.com>
2014-05-31 11:31:52 +0400

    Improved efficient of Bubble Sort

--- algorithms/sorting/bubble_sort.js
+++ algorithms/sorting/bubble_sort.js
@@ -25,16 +25,21 @@ var Comparator = require('../../util/comparator');
 /**
  * Bubble sort algorithm O(n^2)
  */
-var bubbleSort = function (a, comparatorFn) {
-  var comparator = new Comparator(comparatorFn);
-  for (var i = 0; i < a.length; i++) {
-    for (var j = i; j < a.length; j++) {
-      if (comparator.greaterThan(a[i], a[j])) {
-        var tmp = a[i];
-        a[i] = a[j];
-        a[j] = tmp;
+var bubbleSort = function(a, comparatorFn) {
+  var comparator = new Comparator(comparatorFn),
+    n = a.length,
+    bound = n - 1;
+  for (var i = 0; i < n - 1; i++) {
+    var newbound = 0;
+    for (var j = 0; j < bound; j++) {
+      if (comparator.greaterThan(a[j], a[j + 1])) {
+        var tmp = a[j];
+        a[j] = a[j + 1];
+        a[j + 1] = tmp;
+        newbound = j;
       }
     }
+    bound = newbound;
   }
 
   return a;
