4b3b8f780e3f87729dd4372321e5101364cb6c18
dingshu <ds303077135@gmail.com>
2014-06-02 07:58:15 +0800

    small fix

--- algorithms/graph/SPFA.js
+++ algorithms/graph/SPFA.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2014 Felipe Ribeiro
+ * Copyright (C) 2014 Shu Ding
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
