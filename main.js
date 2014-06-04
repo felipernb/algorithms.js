18099e17f11da6b31a03b217ff06d92cd23d2f28
Felipe Ribeiro <felipernb@gmail.com>
2014-06-05 00:05:36 +0200

    Expose Hash Table in main.js

--- main.js
+++ main.js
@@ -54,6 +54,7 @@ var lib = {
   DataStructure: {
     BST: require('./data_structures/bst'),
     Graph: require('./data_structures/graph'),
+    HashTable: require('./data_structures/hash_table'),
     Heap: require('./data_structures/heap'),
     LinkedList: require('./data_structures/linked_list'),
     PriorityQueue: require('./data_structures/priority_queue'),
