399eb4ee8095f2474bc912fa8ff567924b8d5421
nitinsaroha <nitinsaroha6@hotmail.com>
2014-06-06 13:38:24 +0530

    Fix indentation issue

--- algorithms/sorting/selection_sort.js
+++ algorithms/sorting/selection_sort.js
@@ -26,16 +26,16 @@
 var selectionSort = function(a) {
   var n = a.length;
   for (var i = 0; i < n - 1; i++){
-	var min = i;
-	for (var j = i + 1; j < n; j++){
-	  if(a[min] > a[j])
-	    min = j;
-	}
-	if (min != i){
-	  var tmp = a[i];
-	  a[i] = a[min];
-	  a[min] = tmp;
-	}
+    var min = i;
+    for (var j = i + 1; j < n; j++){
+      if(a[min] > a[j])
+        min = j;
+    }
+    if (min != i){
+      var tmp = a[i];
+      a[i] = a[min];
+      a[min] = tmp;
+    }
   }
 
   return a;
