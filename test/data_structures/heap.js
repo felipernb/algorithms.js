eed6f4947736d06df75e8f747d2d115958796bfd
Joshua Curl <curljosh@msu.edu>
2014-08-14 17:45:02 -0400

    Fixed syntax problems

--- test/data_structures/heap.js
+++ test/data_structures/heap.js
@@ -53,14 +53,15 @@ describe('Min Heap', function () {
 
     assert(h.isEmpty());
   });
-  it('should perform a function to all elements from smallest to largest' + 
+
+  it('should perform a function to all elements from smallest to largest' +
      ' with forEach', function () {
     var h = new heap.MinHeap();
     h.heapify([3, 10, 1000, 0, 2, 1]);
 
     var output = [];
     h.forEach(function (n) {
-        output.push(n);
+      output.push(n);
     });
 
     assert.deepEqual(output, [0, 1, 2, 3, 10, 1000]);
@@ -118,14 +119,14 @@ describe('Max Heap', function () {
     assert(h.isEmpty());
   });
 
-  it('should perform a function to all elements from largest to smallest' + 
+  it('should perform a function to all elements from largest to smallest' +
      ' with forEach', function () {
     var h = new heap.MaxHeap();
     h.heapify([3, 10, 1000, 0, 2, 1]);
 
     var output = [];
     h.forEach(function (n) {
-        output.push(n);
+      output.push(n);
     });
 
     assert.deepEqual(output, [1000, 10, 3, 2, 1, 0]);
