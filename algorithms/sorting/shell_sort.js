86e80f7118f311eb6d26ac5689d263abe3af583a
gyj <505324327@qq.com>
2014-08-15 20:09:07 +0800

    fix the linting errors

--- algorithms/sorting/shell_sort.js
+++ algorithms/sorting/shell_sort.js
@@ -3,25 +3,27 @@ var Comparator = require('../../util/comparator');
 /**
  * shell sort  worst:O(n lg n)  best:O(n)
  */
-var shellSort = function(array, comparatorFn) {
+var shellSort = function (array, comparatorFn) {
 	var comparator = new Comparator(comparatorFn),
 		begin = 0,
-		end = array.length-1,
-		gap = parseInt((end-begin+1)/2),
-		i = 0,j = 0,temp = 0;
+		end = array.length - 1,
+		gap = parseInt((end - begin + 1) / 2),
+		i = 0, j = 0, temp = 0;
 
-	while(gap >= 1) {
-		for(i = begin+gap;i <= end;i += 1) {
+	while (gap >= 1) {
+		for (i = begin + gap;i <= end;i += 1) {
 			temp = array[i];
-			j = i-gap;
-			while(j >= begin && comparator.greaterThan(array[j],temp)) {
-				array[j+gap] = array[j];
-				j = j-gap;
+			j = i - gap;
+			while (j >= begin && comparator.greaterThan(array[j], temp)) {
+				array[j + gap] = array[j];
+				j = j - gap;
 			}
-			array[j+gap] = temp;
+			array[j + gap] = temp;
 		}
-		gap = parseInt(gap/2);
+		gap = parseInt(gap / 2);
 	}
+
 	return array;
 };
+
 module.exports = shellSort;
