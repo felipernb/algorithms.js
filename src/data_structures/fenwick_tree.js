9bb0bb95ea0bcc6678bb0a17c0567b1232598974
Juan Lopes <me@juanlopes.net>
2015-02-13 15:36:22 -0200

    fixing spacing

--- src/data_structures/fenwick_tree.js
+++ src/data_structures/fenwick_tree.js
@@ -4,25 +4,25 @@
  * Fenwick Tree (Binary Indexed Tree)
  */
 function FenwickTree(length) {
-  this._elements = new Array(length+1);
-  for(var i=0; i<this._elements.length; i++)
+  this._elements = new Array(length + 1);
+  for (var i = 0; i < this._elements.length ; i++)
     this._elements[i] = 0;
 }
 
 /**
  * Adds value to the array at specified index in O(log n)
  */
-FenwickTree.prototype.adjust = function(index, value) {
-  for(; index < this._elements.length; index += (index&-index))
+FenwickTree.prototype.adjust = function (index, value) {
+  for (; index < this._elements.length ; index += (index&-index))
     this._elements[index] += value;
 };
 
 /**
 * Returns the sum of all value up to specified index in O(log n)
 */
-FenwickTree.prototype.prefixSum = function(index) {
+FenwickTree.prototype.prefixSum = function (index) {
   var sum = 0;
-  for(; index > 0; index -= (index&-index))
+  for (; index > 0 ; index -= (index&-index))
     sum += this._elements[index];
   return sum;
 };
@@ -30,8 +30,8 @@ FenwickTree.prototype.prefixSum = function(index) {
 /**
 * Returns the sum of all value between two indexes in O(log n)
 */
-FenwickTree.prototype.rangeSum = function(fromIndex, toIndex) {
-  return this.prefixSum(toIndex) - this.prefixSum(fromIndex-1);
+FenwickTree.prototype.rangeSum = function (fromIndex, toIndex) {
+  return this.prefixSum(toIndex) - this.prefixSum(fromIndex - 1);
 };
 
 module.exports = FenwickTree;
--- src/test/data_structures/fenwick_tree.js
+++ src/test/data_structures/fenwick_tree.js
@@ -18,10 +18,10 @@ describe('FenwickTree', function () {
     assert.equal(tree.prefixSum(4), 0);
     assert.equal(tree.prefixSum(5), 42);
     assert.equal(tree.prefixSum(6), 42);
-    assert.equal(tree.prefixSum(7), 42+43);
-    assert.equal(tree.prefixSum(8), 42+43);
-    assert.equal(tree.prefixSum(9), 42+43+44);
-    assert.equal(tree.prefixSum(10), 42+43+44);
+    assert.equal(tree.prefixSum(7), 42 + 43);
+    assert.equal(tree.prefixSum(8), 42 + 43);
+    assert.equal(tree.prefixSum(9), 42 + 43 + 44);
+    assert.equal(tree.prefixSum(10), 42 + 43 + 44);
   });
 
   it('should allow range queries', function () {
@@ -31,7 +31,7 @@ describe('FenwickTree', function () {
     tree.adjust(9, 44);
 
 
-    assert.equal(tree.rangeSum(6, 10), 43+44);
-    assert.equal(tree.rangeSum(5, 7), 42+43);
+    assert.equal(tree.rangeSum(6, 10), 43 + 44);
+    assert.equal(tree.rangeSum(5, 7), 42 + 43);
   });
 });
