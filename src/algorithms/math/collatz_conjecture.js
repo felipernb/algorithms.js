f237b8c3ef03a84387ebc9e2fc4bf231a9c32409
Evandro Eisinger <evandroeisinger@gmail.com>
2015-08-24 20:47:43 -0300

    added collatz conjecture algorithm

---
+++ src/algorithms/math/collatz_conjecture.js
@@ -0,0 +1,42 @@
+'use strict';
+
+// cache algorithm results
+var cache = {1: 1};
+
+/**
+ * Collatz Conjecture algorithm
+ *
+ * @param Number
+ * @return Number
+ */
+
+function calculateCollatzConjecture(number) {
+  if (number in cache) return cache[number];
+  if (number % 2 === 0) return cache[number] = number >> 1;
+
+  cache[number] = number * 3 + 1;
+}
+
+/**
+ * Generate Collatz Conjecture
+ *
+ * @param Number
+ * @return Array
+ */
+
+function generateCollatzConjecture(number) {
+  var collatzConjecture = [];
+
+  do {
+    number = calculateCollatzConjecture(number);
+    collatzConjecture.push(number);
+  } while (number !== 1)
+
+  return collatzConjecture;
+}
+
+// export Collatz Conjecture methods
+module.exports = {
+  generate: generateCollatzConjecture,
+  calculate: calculateCollatzConjecture,
+};
