85dba035b900f5aa5ce65b01a6e147e8c3c9ad34
Evandro Eisinger <evandroeisinger@gmail.com>
2015-08-31 11:52:18 -0300

    improved algorithm readability and added a better documentation

--- src/algorithms/sorting/short_bubble_sort.js
+++ src/algorithms/sorting/short_bubble_sort.js
@@ -9,19 +9,25 @@ var Comparator = require('../../util/comparator');
 
 function shortBubbleSort(array, comparatorFn) {
   var comparator = new Comparator(comparatorFn);
-  var length = array.length;
+  var length = array.length - 1;
   var i = 0;
 
   for (i; i < length; i++) {
     var current = array[i];
     var next = array[i+1];
 
-    if (next === undefined) break;
-    if (comparator.lessThanOrEqual(current, next)) continue;
+    /**
+     * If the current value if greater than the next:
+     * - set current value to next value;
+     * - and set next value to current value;
+     * - then reset iterator counter to scan for values to be sorted.
+     */
 
-    array[i+1] = current;
-    array[i] = next;
-    i = -1;
+    if (comparator.greaterThan(current, next)) {
+      array[i+1] = current;
+      array[i] = next;
+      i = -1;
+    }
   }
 
   return array;
