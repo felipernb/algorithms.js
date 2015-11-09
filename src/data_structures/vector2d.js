78d9d8104c8dfaddc47d02b9c3c6487d4fd06d34
be5invis <belleve@typeof.net>
2015-11-10 04:11:13 +0800

    Basic 2D vector operations.

---
+++ src/data_structures/vector2d.js
@@ -0,0 +1,22 @@
+function Vector2D(x, y){
+	this.x = x;
+	this.y = y;
+}
+Vector2D.prototype.add = function(that){
+	return new Vector2D(this.x + that.x, this.y + that.y);
+}
+Vector2D.prototype.minus = function(that){
+	return new Vector2D(this.x - that.x, this.y - that.y);
+}
+Vector2D.prototype.scale = function(amount){
+	return new Vector2D(this.x * amount, this.y * amount);
+}
+Vector2D.prototype.dot = function(that){
+	return this.x * that.x + this.y * that.y
+}
+Vector2D.prototype.cross = function(that){
+	return this.x * that.y - this.y * that.x
+}
+
+
+module.exports = Vector2D
