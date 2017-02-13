945aaab486173c198335e51a1a3b96051c2c1e5f
Felipe Ribeiro <felipernb@gmail.com>
2017-02-13 14:19:19 +0100

    Stop using inheritance wrongly

--- src/data_structures/stack.js
+++ src/data_structures/stack.js
@@ -1,26 +1,53 @@
 'use strict';
-
-var Queue = require('./queue');
+var LinkedList = require('./linked_list');
 
 /**
  * Stack (LIFO) using a Linked List as basis
  */
 function Stack() {
-  Queue.call(this);
+  this._elements = new LinkedList();
+
+  Object.defineProperty(this, 'length', {
+    get: (function() {
+      return this._elements.length;
+    }).bind(this)
+  });
 }
 
-/**
- * Use a Queue as prototype and just overwrite
- * the push method to insert at the 0 position
- * instead of the end of the queue
- */
-Stack.prototype = new Queue();
+Stack.prototype.isEmpty = function() {
+  return this._elements.isEmpty();
+};
 
 /**
- * Adds element to the top of the stack
+ * Adds element to the end of the stack
  */
 Stack.prototype.push = function(e) {
-  this._elements.add(e, 0);
+  this._elements.add(e);
+};
+
+/**
+ * Pops the element from the end of the stack
+ */
+Stack.prototype.pop = function() {
+  if (this.isEmpty()) {
+    throw new Error('Empty queue');
+  }
+
+  var e = this._elements.tail;
+  this._elements.delNode(e);
+  return e.value;
+};
+
+Stack.prototype.peek = function() {
+  if (this.isEmpty()) {
+    throw new Error('Empty queue');
+  }
+
+  return this._elements.tail.value;
+};
+
+Stack.prototype.forEach = function(fn) {
+  this._elements.forEach(fn);
 };
 
 module.exports = Stack;
