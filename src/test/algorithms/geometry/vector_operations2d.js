0954867f403f3af647499dfa51057620df4c962c
Matheus Dall'Rosa <matheusdallrosa94@gmail.com>
2017-09-07 19:36:57 -0300

    Add tests for 2d vector operations

---
+++ src/test/algorithms/geometry/vector_operations2d.js
@@ -0,0 +1,34 @@
+const vectorOp = require('../../../algorithms/geometry/vector_operations2d');
+const assert = require('assert');
+
+describe('VectorOperations', () => {
+  it('crossProduct: {-1,7}x{-5,8}', () => {
+    assert.equal(vectorOp.
+                  crossProduct({x: -1, y: 7}, {x: -5, y: 8}), 27);
+  });
+
+  it('crossProduct: {45,45}x{-45,-45}', () => {
+    assert.equal(vectorOp.
+                  crossProduct({x: 45, y: 45}, {x: -45, y: -45}), 0);
+  });
+
+  it('crossProduct: {1,1}x{1,0}', () => {
+    assert.equal(vectorOp.
+                  crossProduct({x: 1, y: 1}, {x: 1, y: 0}), -1);
+  });
+
+  it('isClockwise: {0,0},{2,2},{0,2}', () => {
+    assert.equal(vectorOp.
+                  isClockwise({x: 0, y: 0}, {x: 2, y: 2}, {x: 0, y: 2}), false);
+  });
+
+  it('isClockwise: {0,0},{2,2},{0,-2}', () => {
+    assert.equal(vectorOp.
+                  isClockwise({x: 0, y: 0}, {x: 2, y: 2}, {x: 0, y: -2}), true);
+  });
+
+  it('isClockwise: {0,0},{2,2},{1,1}', () => {
+    assert.equal(vectorOp.
+                  isClockwise({x: 0, y: 0}, {x: 2, y: 2}, {x: 1, y: 1}), false);
+  });
+});
