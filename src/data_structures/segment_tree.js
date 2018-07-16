ec190a8bba4674b24a1a4870f02761f98bab31fc
Prakash Jha <only2prakash@gmail.com>
2018-07-16 13:21:44 +0530

    Segment Tree
    Separate classes for each min,max and sum function.

--- src/data_structures/segment_tree.js
+++ src/data_structures/segment_tree.js
@@ -1,86 +1,87 @@
 /**
-* Segment Tree 
+* Segment Tree
 *
-* Segment Tree helps to perform various function over a range efficiently in O(log n) time.
+* Segment Tree helps to perform various function over a range efficiently
+* in O(log n) time.
 *
-* In addition to functions that can be implemented by Fenwick Tree, Segment Tree can even perform functions such as
-* minimum or maximum over a range, which cannot be answered by Fenwick Tree.
+* In addition to functions that can be implemented by Fenwick Tree, 
+* Segment Tree can even perform functions such as minimum or maximum 
+* over a range, which cannot be answered by Fenwick Tree.
 *
-* More complicated functions can be done in logarithmic time by doing lazy modification, however the implementation
-* depends on type of function and hence is not implemented here.
+* More complicated functions can be done in logarithmic time by doing 
+* lazy modification, however the implementation depends on type of function
+* and hence is not implemented here.
 *
-* Segment Tree is similiar to binary tree. The element value is stored in leaf nodes and value of function from range[l,r]
-* is stored in internal nodes.
+* Segment Tree is similiar to binary tree. The element value is stored in
+* leaf nodes and value of function from range[l,r] is stored in internal nodes.
 *
-* The root node i.e. index 1 stores the value by performing function over entire range. Similarly other index stores value
-* by performing function in range of its subtree.
-*/
+* The root node i.e. index 1 stores the value by performing function over
+* entire range. Similarly other index stores value by performing function 
+* in range of its subtree.
+* /
 
-class SegmentTree {
+class SumSegmentTree {
+  // Creates an empty segment tree with capacity = length
   constructor(length) {
-    this._size = length;
-    this._elementsSum = new Array(length * 2);
-    this._elementsMin = new Array(length * 2);
-    this._elementsMax = new Array(length * 2);
-
-    for (let i = 0; i < this._elementsSum.length; i++) {
-      this._elementsSum[i] = 0;
-      this._elementsMin[i] = 0;
-      this._elementsMax[i] = 0;
-    }
+      this._baseValue = 0;
+      this._size = length;
+      this._tree = new Array(length*2);
+      for (let i = 0; i < this._tree.length; i++) {
+          this._tree[i] = 0;
+      }
   }
-
-  /**
-  Updates the value at index with the given value
-  */
+  
+  // Commutative binary function to be performed over values
+  func(value1, value2) {
+      return (value1 + value2);
+  }
+  
+  // Changes the element at given index to be equal to given value
   modify(index, value) {
-    index = index + this._size;
-
-    this._elementsSum[index] = value;
-    this._elementsMin[index] = value;
-    this._elementsMax[index] = value;
-    for (let i = index; i > 1; i >>= 1) {
-      this._elementsSum[i >> 1] = (this._elementsSum[i] + this._elementsSum[i ^ 1]);
-      this._elementsMin[i >> 1] = Math.min(this._elementsMin[i], this._elementsMin[i ^ 1]);
-      this._elementsMax[i >> 1] = Math.max(this._elementsMax[i], this._elementsMax[i ^ 1]);
-    }
+      let idx = index + this._size;
+      this._tree[idx] = value;
+      for (let i = idx; i > 1; i >>= 1) {
+          this._tree[ i >> 1 ] = this.func(this._tree[i], this._tree[i^1]);
+      }
   }
-
-  /**
-  Return the sum of all elements in the range [from, to]
-  */
-  getSum(from, to) {
-    let ans = 0;
-    for (from += this._size, to += this._size + 1; from < to; from >>= 1, to >>= 1) {
-      if (from % 2 == 1) ans += this._elementsSum[from++];
-      if (to % 2 == 1) ans += this._elementsSum[--to];
-    }
-    return ans;
-  }
-
-  /**
-  Return the minimum of all elements in the range [from, to]
-  */
-  getMin(from, to) {
-    let ans = Number.MAX_SAFE_INTEGER;
-    for (from += this._size, to += this._size + 1; from < to; from >>= 1, to >>= 1) {
-      if (from % 2 == 1) ans = Math.min(ans, this._elementsMin[from++]);
-      if (to % 2 == 1) ans = Math.min(ans, this._elementsMin[--to]);
-    }
-    return ans;
-  }
-
-  /**
-  Return the maximum of all elements in the range [from, to]
-  */
-  getMax(from, to) {
-    let ans = Number.MIN_SAFE_INTEGER;
-    for (from += this._size, to += this._size + 1; from < to; from >>= 1, to >>= 1) {
-      if (from % 2 == 1) ans = Math.max(ans, this._elementsMax[from++]);
-      if (to % 2 == 1) ans = Math.max(ans, this._elementsMin[--to]);
-    }
-    return ans;
+  
+  // Returns the value by performing the binary function in range = [from,to]
+  get(from, to) {
+      let ans = this._baseValue;
+      let l = from + this._size;
+      let r = to + this._size + 1;
+      for (; l < r; l >>= 1, r >>= 1) {
+          if (l&1) ans = this.func(ans, this._tree[l++]);
+          if (r&1 ) ans = this.func(ans, this._tree[--r]);
+      }
+      return ans;
   }
 }
 
-module.exports = SegmentTree;
+class MinSegmentTree extends SumSegmentTree {
+  constructor(length) {
+      super(length);
+      this._baseValue = Number.MAX_SAFE_INTEGER;
+  }
+
+  func(value1, value2) {
+      return Math.min(value1, value2);
+  }
+}
+
+class MaxSegmentTree extends SumSegmentTree {
+  constructor(length) {
+      super(length);
+      this._baseValue = Number.MIN_SAFE_INTEGER;
+  }
+
+  func(value1, value2) {
+      return Math.max(value1, value2);
+  }
+}
+
+module.exports = {
+  SumSegmentTree,
+  MinSegmentTree,
+  MaxSegmentTree
+};
