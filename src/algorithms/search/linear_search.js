b26e607f43c8351211c861c0f3485c7cb15ba08c
nersus15 <38497343+nersus15@users.noreply.github.com>
2019-10-27 00:15:46 +0800

    Update linear_search.js

--- src/algorithms/search/linear_search.js
+++ src/algorithms/search/linear_search.js
@@ -1,7 +1,7 @@
-
-  module.export = linearSearch = () => {
+const linearSearch = (arr, val) => {
     for ( var i = 0; i < arr.length; i++ ) {
         if ( arr[i] === val ) return i;
     }
     return -1;
 }
+module.export = 
