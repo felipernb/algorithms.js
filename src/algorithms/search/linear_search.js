f233865dd7abd25a4f3871bd5910e1722a25595e
nersus15 <38497343+nersus15@users.noreply.github.com>
2019-10-27 00:59:03 +0800

    Update linear_search.js

--- src/algorithms/search/linear_search.js
+++ src/algorithms/search/linear_search.js
@@ -1,5 +1,5 @@
 const linearSearch = (arr, val) => {
-    for ( var i = 0; i < arr.length; i++ ) {
+    for ( let i = 0; i < arr.length; i++ ) {
         if ( arr[i] === val ) return i;
     }
     return -1;
